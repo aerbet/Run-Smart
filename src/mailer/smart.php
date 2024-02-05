89575ecdec1161f39131f2c4eb636ba25cb4a1c0
Erbol Zhaparov <ae270718@gmail.com>
2024-02-05 14:16:46 +0600

    fixed some things:)

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'worjd.07@gmail.com';                 // Наш логин
-$mail->Password = 'zqrluvukesoqgqni';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                   // TCP port to connect to
  
-$mail->setFrom('worjd.07@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('', 'Pulse');   // От кого письмо
 $mail->addAddress('nepexox645@carpetra.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
